e72e7c2c7cb9b9af3057232c52e54563a679b79d
Muneer Hussain <97391190+muneer-hussain@users.noreply.github.com>
2022-03-01 11:30:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let enter = document.querySelector("#input")
+let language = document.querySelector("#lang-name")
+let btn = document.querySelector("#btn-enter");
+
+language.value = "en-us";
+
+function textToSpeech(text) {
+    let speech = new SpeechSynthesisUtterance(text)
+    speechSynthesis.speak(speech);
+
+}
+btn.addEventListener("click", function() {
+    if (enter.value != "") {
+        textToSpeech(enter.value)
+    }
+})
